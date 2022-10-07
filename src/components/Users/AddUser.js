1a472575c10c91fc5b27d9cfe8fbd9a0aef8f513
LusineTorosian <lusinetorosyan.evistep@gmail.com>
2022-10-07 17:41:13 +0400

    add portal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
-import classes from './AddUser.module.css';
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
+import classes from './AddUser.module.css';
 
 
 const AddUser = (props) => {
@@ -47,7 +48,7 @@ const AddUser = (props) => {
     };
 
     return (
-        <div>
+        <Wrapper>
             {error && (
                 <ErrorModal 
                     title={error.title} 
@@ -64,7 +65,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     );
 };
 
